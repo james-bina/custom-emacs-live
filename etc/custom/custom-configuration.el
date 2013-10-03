a0e75cc62a1b8a6974acab56594ec32272bc3b24
James Warren <james.warren@binatechnologies.com>
2013-10-03 16:03:43 -0700

    theme customizations

--- etc/custom/custom-configuration.el
+++ etc/custom/custom-configuration.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (jw-purple-haze)))
  '(custom-safe-themes (quote ("44dc0ca6175d2a0532e0e6ebb68a2e1831824fa69b2b362c7592aa61e63c0f04" default)))
  '(sclang-auto-scroll-post-buffer t)
  '(sclang-eval-line-forward nil))
@@ -17,6 +18,6 @@
  '(ediff-even-diff-B ((((class color) (background dark)) (:background "dark red"))))
  '(ediff-odd-diff-A ((((class color) (background dark)) (:background "dark green"))))
  '(ediff-odd-diff-B ((((class color) (background dark)) (:background "dark red"))))
- '(eval-sexp-fu-flash ((((class color) (background dark)) (:background "grey15" :foreground "DeepPink3"))))
- '(mumamo-background-chunk-major ((((class color) (background dark)) (:background "black"))))
- '(mumamo-background-chunk-submode1 ((((class color) (background dark)) (:background "black")))))
+ '(eval-sexp-fu-flash ((((class color) (background dark)) (:background "grey15" :foreground "DeepPink3"))) t)
+ '(mumamo-background-chunk-major ((((class color) (background dark)) (:background "black"))) t)
+ '(mumamo-background-chunk-submode1 ((((class color) (background dark)) (:background "black"))) t))
