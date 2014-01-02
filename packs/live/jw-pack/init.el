fd15aa5ea02dc37383c8f142dc31753957580683
James Warren <james.warren@binatechnologies.com>
2014-01-02 13:02:02 -0800

    typo fix

--- packs/live/jw-pack/init.el
+++ packs/live/jw-pack/init.el
@@ -21,7 +21,6 @@
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 (add-hook 'cider-repl-mode-hook 'paredit-mode)
 
-(setq nrepl-hide-special-buffers t)
+;(setq nrepl-hide-special-buffers t)
 (setq cider-repl-popup-stacktraces t)
-(setq nrepl-buffer-name-separator t)
 (setq cider-repl-display-in-current-window t)
